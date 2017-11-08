4920118886d349cfc029359a673363b9e30aefd9
temp <temp>
2017-11-08 12:28:29 -0500

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redPictureBox = new System.Windows.Forms.PictureBox();
+            this.bluePictureBox = new System.Windows.Forms.PictureBox();
+            this.yellowPictureBox = new System.Windows.Forms.PictureBox();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.picturesLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.redPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Segoe Script", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabel.Location = new System.Drawing.Point(25, 20);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(583, 267);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // redPictureBox
+            // 
+            this.redPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.redPictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redPictureBox.BackgroundImage")));
+            this.redPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.redPictureBox.Location = new System.Drawing.Point(24, 353);
+            this.redPictureBox.Name = "redPictureBox";
+            this.redPictureBox.Size = new System.Drawing.Size(80, 58);
+            this.redPictureBox.TabIndex = 3;
+            this.redPictureBox.TabStop = false;
+            // 
+            // bluePictureBox
+            // 
+            this.bluePictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.bluePictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bluePictureBox.BackgroundImage")));
+            this.bluePictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bluePictureBox.Location = new System.Drawing.Point(47, 310);
+            this.bluePictureBox.Name = "bluePictureBox";
+            this.bluePictureBox.Size = new System.Drawing.Size(42, 43);
+            this.bluePictureBox.TabIndex = 4;
+            this.bluePictureBox.TabStop = false;
+            // 
+            // yellowPictureBox
+            // 
+            this.yellowPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.yellowPictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("yellowPictureBox.BackgroundImage")));
+            this.yellowPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.yellowPictureBox.Location = new System.Drawing.Point(38, 417);
+            this.yellowPictureBox.Name = "yellowPictureBox";
+            this.yellowPictureBox.Size = new System.Drawing.Size(51, 37);
+            this.yellowPictureBox.TabIndex = 5;
+            this.yellowPictureBox.TabStop = false;
+            // 
+            // redLabel
+            // 
+            this.redLabel.BackColor = System.Drawing.Color.Transparent;
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(95, 368);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(206, 29);
+            this.redLabel.TabIndex = 6;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.BackColor = System.Drawing.Color.Transparent;
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(95, 321);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(206, 29);
+            this.blueLabel.TabIndex = 7;
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.BackColor = System.Drawing.Color.Transparent;
+            this.yellowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLabel.ForeColor = System.Drawing.Color.White;
+            this.yellowLabel.Location = new System.Drawing.Point(95, 417);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(206, 29);
+            this.yellowLabel.TabIndex = 8;
+            // 
+            // picturesLabel
+            // 
+            this.picturesLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picturesLabel.ForeColor = System.Drawing.Color.Transparent;
+            this.picturesLabel.Location = new System.Drawing.Point(365, 300);
+            this.picturesLabel.Name = "picturesLabel";
+            this.picturesLabel.Size = new System.Drawing.Size(243, 160);
+            this.picturesLabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(631, 469);
+            this.Controls.Add(this.picturesLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.yellowPictureBox);
+            this.Controls.Add(this.bluePictureBox);
+            this.Controls.Add(this.redPictureBox);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.redPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bluePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox redPictureBox;
+        private System.Windows.Forms.PictureBox bluePictureBox;
+        private System.Windows.Forms.PictureBox yellowPictureBox;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.Label picturesLabel;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+
+        // random number generator
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //display initial message and options
+            outputLabel.Text = "You wake up in a mysterious place with lots of old trees, dark sky weird shadows and you can hear whispers. You see a light and an old car. The last you remember was drinking a lot in Mario's party \nWould you go to the light or investigate the car?";
+            redLabel.Text = "Go to the light";
+            blueLabel.Text = "Go to the car";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            double dice1Value = randGen.Next(1, 101);
+
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 1) { scene = 3; }
+                else if (scene == 2)
+                {
+                    if (dice1Value <= 70)
+                    {
+                        scene = 6;
+                    }
+                    else { scene = 5; }
+                }
+                else if (scene == 3) { scene = 4; }
+                else if (scene == 4) { scene = 15; }
+                else if (scene == 16) { scene = 15; }
+                else if (scene == 6) { scene = 11; }
+                else if (scene == 8) { scene = 14; }
+                else if (scene == 10) { scene = 12; }
+                else if (scene == 17) { scene = 18; }
+                else if (scene == 14) { scene = 19; }
+                else if (scene == 5) { scene = 1; }
+                else if (scene == 9) { scene = 1; }
+                else if (scene == 12) { scene = 1; }
+                else if (scene == 13) { scene = 1; }
+                else if (scene == 15) { scene = 1; }
+                else if (scene == 18) { scene = 1; }
+                else if (scene == 19) { scene = 1; }
+                else if (scene == 20) { scene = 1; }
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 2)
+                {
+                    if (dice1Value <= 50)
+                    {
+                        scene = 7;
+                    }
+
+                    else { scene = 5; }
+                }
+                else if (scene == 3) { scene = 20; }
+                else if (scene == 7) { scene = 8; }
+                else if (scene == 8) { scene = 13; }
+                else if (scene == 10) { scene = 13; }
+                else if (scene == 3) { scene = 20; }
+                else if (scene == 4) { scene = 16; }
+                else if (scene == 5) { scene = 21; }
+                else if (scene == 9) { scene = 21; }
+                else if (scene == 12) { scene = 21; }
+                else if (scene == 13) { scene = 21; }
+                else if (scene == 15) { scene = 21; }
+                else if (scene == 18) { scene = 21; }
+                else if (scene == 20) { scene = 21; }
+                else if (scene == 14) { scene = 19; }
+                else if (scene == 6) { scene = 8; }
+                else if (scene == 19) { scene = 21; }
+                else if (scene == 16) { scene = 17; }
+                else if (scene == 17) { scene = 14; }
+
+            }
+            else if (e.KeyCode == Keys.N)  //yellow button press
+            {
+                if (scene == 6)
+                {
+                    if (dice1Value <= 70)
+                    {
+                        scene = 10;
+                    }
+
+                    else { scene = 9; }
+                }
+
+                else if (scene == 7)
+                {
+                    if (dice1Value <= 70)
+                    {
+                        scene = 10;
+                    }
+
+                    else { scene = 9; }
+                }
+                else if (scene == 14) { scene = 19; }
+
+                
+            }
+            switch (scene)
+            {
+
+                case 1: //start scene 
+                    picturesLabel.Image = Properties.Resources._25549;
+                    outputLabel.Text = "You wake up in a mysterious place with lots of old trees, dark sky weird shadows and you can hear some whispers. You see a light and an old car. The last you remember was drinking a lot in Mario's party." + "\nWould you go to the light or investigate the car?";
+                    redLabel.Text = "Go to the light";
+                    blueLabel.Text = "Go to the car";
+                    yellowLabel.Text = "";
+                    break;
+                case 2:
+                    picturesLabel.Image = Properties.Resources.chatillon_car_graveyard_abandoned_cars_cemetery_belgium_31;
+                    outputLabel.Text = "There is a lantern and a screwdriver, but you hear some steps behind you. \nWould you take the lantern or the screwdriver ? ";
+                    redLabel.Text = "Pick the lantern";
+                    blueLabel.Text = "Pick the screwdriver";
+                    yellowLabel.Text = "";
+                    break;
+                case 3:
+                    picturesLabel.Image = Properties.Resources._09c9b295f8ff686b7e8674f9c0151555__lost_river_creepy_pictures;
+                    outputLabel.Text = "As you get closer you hear shouts, people crying. You see a river and a big tree with a branch that almost cross over the river. \nWhat would you do? ";
+                    redLabel.Text = "Try to cross the river";
+                    blueLabel.Text = "Cross it through the branch";
+                    yellowLabel.Text = "";
+                    break;
+                case 4:
+                    picturesLabel.Image = Properties.Resources._1dae12354aea3992aa7e90478ac81a51;
+                    outputLabel.Text = "Nothing happened in the river, you are closer to the noise and the light. " + "\nWould you continue to the light or go where the noise come from ? ";
+                    redLabel.Text = "Go to the light";
+                    blueLabel.Text = "Go to the noise";
+                    yellowLabel.Text = "";
+                    break;
+                case 5:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "The black shadow jump to you and bite your neck before you can even react \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 6:
+                    picturesLabel.Image = Properties.Resources.bdacea4eb922cfae8ebf56dde1c2c12e__creepy_things_bridges;
+                    outputLabel.Text = "You turn around with the lantern and the beast runs away scared of the light. You see an old path between the grass and a old wooden bridge that cross over a gorge, but there are some shiny eyes looking at you \nWould you walk trought the path or still searching in the car? ";
+                    redLabel.Text = "Path";
+                    blueLabel.Text = "Still searching";
+                    yellowLabel.Text = "Bridge";
+                    break;
+                case 7:
+                    picturesLabel.Image = Properties.Resources.bdacea4eb922cfae8ebf56dde1c2c12e__creepy_things_bridges;
+                    outputLabel.Text = "You stick the screwdriver in the beast and it disappear.You see an old path between the grass and a old wooden bridge that cross over a gorge, but there are some shiny eyes looking at you \nWould you walk trought  the path or still searching in the car? ";
+                    redLabel.Text = "Path";
+                    blueLabel.Text = "Still searching";
+                    yellowLabel.Text = "Bridge";
+                    break;
+                case 8:
+                    outputLabel.Text = "You found a key and you decided to keep it and then you start walking theway marked with little stones. You get to the end of the path and there's a hole but can't see anything inside \nWould you jump or look around ? ";
+                    redLabel.Text = "Jump";
+                    blueLabel.Text = "Look around";
+                    yellowLabel.Text = "";
+                    break;
+                case 9:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "You fall off the bridge and sleep forever \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 10:
+                    outputLabel.Text = "You get to the end of the bridge and there's a hole but can't see anything inside\nWould you jump or look around ? ";
+                    redLabel.Text = "Jump";
+                    blueLabel.Text = "Look around";
+                    yellowLabel.Text = "";
+                    break;
+                case 11:
+                    outputLabel.Text = "After avoiding the big hole, You get to the end of the path and there's a hole but can't see anything inside \nWould you jump or look around ? ";
+                    redLabel.Text = "Jump";
+                    blueLabel.Text = "Look around";
+                    yellowLabel.Text = "";
+                    break;
+                case 12:
+                    outputLabel.Text = "You get to a white room with a door and nothing else, where you starve to death \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 13:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "You start looking around but a big soul that crosses you and let you in the floor bleeding badly where you bleed out \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 14:
+                    outputLabel.Text = "You appear in a white room with a door and nothing else, and you open it with the key you picked before. Suddenly every thing is black but you open your eyes and you find yourself lay in a hospital bed with all your family around you, Well done! You wake from the coma \nYou can cry, say hello or ask to sleep 5 more minutes because you are tired, what would you do? ";
+                    redLabel.Text = "Cry";
+                    blueLabel.Text = "Hello";
+                    yellowLabel.Text = "Sleep";
+                    break;
+                case 15:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "You died after two days in coma by alcohol poisoning \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 16:
+                    outputLabel.Text = "A strong wind start blocking you to continue while the screams sound closer \nDo you give up or still trying to continue?";
+                    redLabel.Text = "Give up";
+                    blueLabel.Text = "Continue";
+                    yellowLabel.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "You get to a room with two rooms, one is red and the other is blue \nRed door or blue door?";
+                    redLabel.Text = "Red door";
+                    blueLabel.Text = "Blue door";
+                    yellowLabel.Text = "";
+                    break;
+                case 18:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "You fall forever \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 19:
+                    outputLabel.Text = "You win! Do you want to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 20:
+                    picturesLabel.Image = Properties.Resources._5gwDEpSj;
+                    outputLabel.Text = "The branch wasn't strong enough so you fall and don't wake up \nWould you like to play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    yellowLabel.Text = "";
+                    break;
+                case 21:
+                    outputLabel.Text = "Thank you for playing!";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    yellowLabel.Text = "";
+                    break;
+                default:
+                    outputLabel.Text = "error";
+                    break;
+            }
+
+        }
+    }
+}
